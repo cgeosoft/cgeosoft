bb496e56279b64b9f80d43a605a4988223e1091f
Christos Georgiou <georgiou.christos@outlook.com>
2018-07-04 11:23:41 +0300

    update packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,23 @@
-const extend = require('extend')
 const gulp = require('gulp')
-const toolkit = require('gulp-cg-toolkit')
-toolkit.extendTasks(gulp)
+require('gulp-cg-toolkit')(gulp, {
+  languages: ['en'],
+  dirs: {
+    root: './',
+    dist: './dist',
+    src: './src',
+    scss: './src/scss',
+    statics: [{
+      source: './src/static/**/*.*',
+      destination: './dist/'
+    }, {
+      source: './node_modules/devicon/devicon.min.css',
+      destination: './dist/devicon/'
+    }, {
+      source: './node_modules/devicon/fonts/**/*.*',
+      destination: './dist/devicon/fonts'
+    }]
+  },
+  handlebars: {
+    helpers: {}
+  }
+})
