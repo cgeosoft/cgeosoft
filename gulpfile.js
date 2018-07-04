8c9ab106c5990d9c0da94f4431bbe06ed39cf735
Christos Georgiou <georgiou.christos@outlook.com>
2018-07-04 12:08:55 +0300

    fix config

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,13 @@
 const gulp = require('gulp')
 require('gulp-cg-toolkit')(gulp, {
-  languages: ['en'],
-  dirs: {
-    root: './',
-    dist: './dist',
-    src: './src',
-    scss: './src/scss',
-    statics: [{
-      source: './src/static/**/*.*',
-      destination: './dist/'
-    }, {
-      source: './node_modules/devicon/devicon.min.css',
-      destination: './dist/devicon/'
-    }, {
-      source: './node_modules/devicon/fonts/**/*.*',
-      destination: './dist/devicon/fonts'
-    }]
-  },
-  handlebars: {
-    helpers: {}
-  }
+  files: [{
+    source: './src/static/**/*.*',
+    destination: './dist/'
+  }, {
+    source: './node_modules/devicon/devicon.min.css',
+    destination: './dist/'
+  }, {
+    source: './node_modules/devicon/fonts/**/*.*',
+    destination: './dist/fonts/'
+  }]
 })
